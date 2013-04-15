ffaca33edf5639bf50417ede936a7ad213c3f543
D <brezhnd@gmail.com>
2013-04-14 17:43:13 -0700

    instance variables added

--- GGP_Base/src/players/Subplayer.java
+++ GGP_Base/src/players/Subplayer.java
@@ -1,16 +1,20 @@
 package players;
 
-
 import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Role;
 import org.ggp.base.util.statemachine.StateMachine;
 
 public abstract class Subplayer implements Runnable{
-	public Subplayer(final StateMachine stateMachine, Role role, 
-			PlayerResult playerResult, final MachineState currentState){
-		
+	final StateMachine stateMachine;
+	Role role;
+	PlayerResult playerResult;
+	MachineState currentState;
+	
+	public Subplayer(final StateMachine stateMachine, Role role,
+			PlayerResult playerResult, MachineState currentState) {
+		this.stateMachine = stateMachine;
+		this.role = role;
+		this.playerResult = playerResult;
+		this.currentState = currentState;
 	}
-	
-	
-	
 }
